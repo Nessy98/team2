f0a66731c7606dd08ce2c3f3986a49a12f0a3ef5
Nessy98 <vanesa.nessi98@gmail.com>
2018-03-28 17:36:50 +0000

    added alarm gui with button

--- alarm/SoundAlarmAction.py
+++ alarm/SoundAlarmAction.py
@@ -1,6 +1,19 @@
+import Tkinter 
+import tkMessageBox
+
 from alarm.AlarmAction import AlarmAction
+
+top = Tkinter.Tk(className = "Alarm")
+
+def callBack():
+    tkMessageBox.showinfo("Alarm", "Alarm Stopped")
+    top.quit()
 
 
 class SoundAlarmAction(AlarmAction):
+
     def do_action(self):
-        print 'ALARMA'
+	B = Tkinter.Button(top, text="Stop Alarm", command = callBack)
+	B.pack()
+	top.mainloop()
+
--- analyzer/PostureAnalyzer.py
+++ analyzer/PostureAnalyzer.py
@@ -1,25 +1,26 @@
 import cv2
+import numpy as np
 
 from analyzer.BaseAnalyzer import BaseAnalyzer
-
 cap = cv2.VideoCapture(0)
 cap.set(5, 1)
 cap.set(3, 480)  # WIDTH
 cap.set(4, 272)  # HEIGHT
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 class PostureAnalyzer(BaseAnalyzer):
+    
     def has_exception(self):
         return not self.__is_person_in_frame()
 
     def __is_person_in_frame(self):
 
-        
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")        
+	ret, frame = cap.read()
+
+    	gray = cv2.cvtColor(frame, 6)
     	personInFrame = face_cascade.detectMultiScale(gray, 1.3, 5)
 	
-	if len(personInframe) == 1:
+	if len(personInFrame) == 1:
 		return True
 	else:
 		return False
